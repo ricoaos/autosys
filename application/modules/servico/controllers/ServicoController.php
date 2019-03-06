e5435cd293b8a8bffbf6a120293bc75bfc8afa53
Ricardo Alexandre <ricardo.alexandre@basis.com.br>
2019-03-06 15:31:09 -0300

    Ajuste de menu e pasta img

---
+++ application/modules/servico/controllers/ServicoController.php
@@ -0,0 +1,46 @@
+<?php
+class Servico_ServicoController extends App_Controller_Action
+{
+	public function init()
+	{
+		$this->idOrganizacao = App_Identity::getOrganizacao();
+	}
+
+	/**
+	 * 
+	 * @throws Exception
+	 */
+	public function indexAction()
+	{
+		
+		
+	}
+	
+	/**
+	 * 
+	 * Enter description here ...
+	 */
+	public function listagemAction()
+	{
+	
+	}
+		
+	/**
+	 * 
+	 * Enter description here ...
+	 */
+	public function inativarregistroAction()
+	{
+
+	}
+		
+	/**
+	 * 
+	 * @param unknown $params
+	 * @return string
+	 */
+	public function getdadoscadastrados($params)
+	{
+
+	}
+}
--- public/js/plugins/cep.js
+++ public/js/plugins/cep.js
@@ -4,22 +4,21 @@
 function getLogradouro(cep){
 	var cep = $('#st_cep').val().replace(/\D/g,"");
     $.ajax({
-        url : baseUrl+'/apoio/cep/getlogbycep',
-        data: 'cep='+cep,
+        url : 'http://cep.republicavirtual.com.br/web_cep.php?cep='+cep+'&formato=json',
         dataType: 'json',
         type:"POST",
         success: function(response){
         	
-        	var objResult = response.result;
+        	var objResult = response;
         	
             if(objResult != ''){
                 $("#st_numero,#st_complemento").attr('readonly',false).css({'background':'#FFF'});
                 $('#st_logradouro').val(objResult.logradouro);
                 $('#st_bairro').val(objResult.bairro);
                 $('#st_cidade').val(objResult.cidade);
-                $('#st_estado').val(objResult.siglaEstado);
-                $("#st_tipo_logradouro").val(objResult.tipologradouro);
-                $("#id_municipio").val(objResult.municipio);
+                $('#st_estado').val(objResult.uf);
+                $("#st_tipo_logradouro").val(objResult.tipo_logradouro);
+                //$("#id_municipio").val(objResult.municipio);
                 $("#st_numero").focus();
             }else{
                 alert("Cep inválido");
