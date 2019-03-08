d27c36c53bef8b9f42cf1f2bdb2059576745ba73
Ricardo Alexandre <ricardo.alexandre@basis.com.br>
2019-03-08 13:30:14 -0300

    TESTE DE CEP

--- public/js/plugins/cep.js
+++ public/js/plugins/cep.js
@@ -2,14 +2,11 @@ function getLogradouro(cep){
 	var cep = $('#st_cep').val().replace(/\D/g,"");
 	
     $.ajax({
-        url : 'https://viacep.com.br/ws/'+cep+'/json/',
+        url : 'http://cep.republicavirtual.com.br/web_cep.php?cep='+cep+'&formato=json',
         dataType: 'json',
-        type:"POST",
         success: function(response){
         	
         	var objResult = response;
-        	
-        	alert(objResult.cep);
 
             if(objResult.resultado != 0){
                 $("#st_numero,#st_complemento").attr('readonly',false).css({'background':'#FFF'});
@@ -23,6 +20,18 @@ function getLogradouro(cep){
             }else{
                 alert(objResult.resultado_txt);
             }
-        }
+        },
+	    error: function (request, status, erro) {
+	    	alert(request.responseText);
+	        alert("Problema ocorrido: " + status + "\nDescição: " + erro);
+	        //Abaixo está listando os header do conteudo que você requisitou, só para confirmar se você setou os header e dataType corretos
+	        alert("Informações da requisição: \n" + request.getAllResponseHeaders());
+	    },
+	    complete: function (jqXHR, textStatus) {
+	        alert("Chegou ao fim: : " + textStatus);
+	        //Exibindo o valor que você obeteve e repassou para o confirmationValue
+	        //Exibindo o valor que você obeteve e repassou para o confirmationValue
+	        alert("Confirmation value: " + confirmationValue);
+	    }
    });
 }
