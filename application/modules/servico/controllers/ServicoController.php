1460f9b450ecf834158a4e65320b8337587d35eb
Ricardo Alexandre <ricardo.alexandre@basis.com.br>
2019-05-28 11:56:29 -0300

    Grud para a funcionalidade de serviços

--- application/modules/servico/controllers/ServicoController.php
+++ application/modules/servico/controllers/ServicoController.php
@@ -4,6 +4,8 @@ class Servico_ServicoController extends App_Controller_Action
 	public function init()
 	{
 		$this->idOrganizacao = App_Identity::getOrganizacao();
+		$this->idUsuario = App_Identity::getIdUsuario();
+		$this->mServico = new Model_Produto_Servico();
 	}
 
 	/**
@@ -12,8 +14,49 @@ class Servico_ServicoController extends App_Controller_Action
 	 */
 	public function indexAction()
 	{
-		
-		
+	    if($this->_request->getParam('id'))
+	    {
+	        list($date,$id) = explode('@',base64_decode($this->_request->getParam('id')));
+	        $args = self::getdadoscadastrados($id);
+	        $this->view->dadospagina = $args;
+	    }
+	    
+	    $post = $this->_request->getPost();
+	    
+	    //Realiza a inserção das informações
+	    if($this->_request->isPost())
+	    {
+	        
+	        try {
+	            $post["st_nome"] = strtoupper($post["st_nome"]);
+	            $post["id_usuario_cadastro"] = $this->idUsuario;
+	            $post["dt_cadastro"] = date('Y-m-d H:i:s');
+	            $post['id_ativo'] = 1;
+	            
+	            if(empty($post['id_servico'])){
+	                
+	                $rsServico = $this->mServico->insert($post);
+	                
+	            }else{
+	                $post['id_ativo'] = empty($post['id_ativo'])? 0 : $post['id_ativo'];
+	                $where = $this->mServico->getAdapter()->quoteInto('id_servico = ?', $post["id_servico"]);
+	                $this->mServico->update($post, $where);
+	                
+	                $rsServico = $post['id_servico'];
+	            }
+	            
+	            $getdados = self::getdadoscadastrados($rsServico);
+	            $this->view->dadospagina = $getdados;
+	            
+	            $msg=2;
+	            
+	        } catch (Zend_Db_Exception $e) {
+	            $e->rollBack();
+	            $msg= $e->getMessage();
+	        }
+	        
+	        $this->view->msg = $msg;
+	    }
 	}
 	
 	/**
@@ -22,7 +65,8 @@ class Servico_ServicoController extends App_Controller_Action
 	 */
 	public function listagemAction()
 	{
-	
+	    $rsServico = $this->mServico->fetchAll()->toArray();
+	    $this->view->rsServico = $rsServico;
 	}
 		
 	/**
@@ -31,7 +75,13 @@ class Servico_ServicoController extends App_Controller_Action
 	 */
 	public function inativarregistroAction()
 	{
-
+	    if($this->_request->getParam('id'))
+	    {
+	        list($date,$id) = explode('@',base64_decode($this->_request->getParam('id')));
+	        $where = $this->mServico->getAdapter()->quoteInto('id_servico = ?', $id );
+	        $this->mServico->update(array('id_ativo'=> 0),$where);
+	        $this->_redirect('servico/servico/listagem');
+	    }
 	}
 		
 	/**
@@ -41,6 +91,7 @@ class Servico_ServicoController extends App_Controller_Action
 	 */
 	public function getdadoscadastrados($params)
 	{
-
+	    $dadospagina = $this->mServico->fetchAll(array('id_servico = ?' => $params))->toArray();
+	    return $dadospagina[0];
 	}
 }
