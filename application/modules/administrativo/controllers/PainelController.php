0800996a6a42ce9f9b657ee39709ec32cb74afe4
Ricardo Alexandre <ricardo.alexandre@basis.com.br>
2019-05-28 14:52:09 -0300

    Adicionando contadores para o painel

--- application/modules/administrativo/controllers/PainelController.php
+++ application/modules/administrativo/controllers/PainelController.php
@@ -2,8 +2,33 @@
 
 class Administrativo_PainelController extends App_Controller_Action
 {
+    public function init()
+    {
+        $this->idOrganizacao = App_Identity::getOrganizacao();
+    }
+    
     public function indexAction()
     {
-    	
+        $mProdutos = new Model_Produto_Produto();
+        $rsProduto = self::getcount($mProdutos);
+        $this->view->rsProduto = $rsProduto;
+        
+        $mCliente = new Model_Cliente_VwCliente();
+        $rsCliente = self::getcount($mCliente,1);
+        $this->view->rsCliente = $rsCliente; 
+        
+    }
+    
+    public function getcount($params, $where=null)
+    {
+        $select = $params->select();
+        $select->from($params,array('count(*) as count'));
+        if($where == 1){
+        
+            $select->where('id_organizacao = ?' , $this->idOrganizacao);
+        
+        }
+        $rows = $params->fetchAll($select);
+        return($rows[0]);
     }
 }
